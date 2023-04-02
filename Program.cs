7a1f31e09874d47e9745dea7db8ca4cb20d11f77
BaoNgo208 <baobap208@gmail.com>
2023-04-02 20:10:20 +0700

    rưer

--- Program.cs
+++ Program.cs
@@ -1,22 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+<<<<<<< Updated upstream
+
+namespace TestGit
+=======
 using System.Windows.Forms;
 using PBL3_QLQuanNet.View;
 namespace PBL3_QLQuanNet
+>>>>>>> Stashed changes
 {
-    internal static class Program
+    internal class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+<<<<<<< Updated upstream
+            Console.WriteLine("hello");
+=======
+<<<<<<< HEAD
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new ClientForm());
+=======
+            Console.WriteLine("hello");
+>>>>>>> 0c6aaed (hello world)
+>>>>>>> Stashed changes
         }
     }
 }
